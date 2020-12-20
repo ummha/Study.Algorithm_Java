f319c6a8f33f45df58aa41a0c17e2c5a45e5f862
dlalstj0213 <dlalstj0213@gmail.com>
2020-12-20 16:41:06 +0900

    버블정렬 분석

---
+++ src/basic/sort/README.md
@@ -0,0 +1,5 @@
+# 시간복잡도
+
+![시간복잡도비교](Study.Algorithm_Java\imgs\time_complexity_comparison.png)
+
+
---
+++ src/basic/sort/_01_bubble/BubbleSort.java
@@ -0,0 +1,34 @@
+package basic.sort._01_bubble;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class BubbleSort {
+	
+	public static void bubbleSort(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			for(int j = 0; j < arr.length - i - 1; j++) {
+				if(arr[j] > arr[j+1]) {
+					int temp = arr[j+1];
+					arr[j+1] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Random generator = new Random(); // 랜덤한 값 추출 객체 생성
+		int arrSize = 10;
+		int[] arrList = new int[arrSize];
+		for(int i = 0; i < arrSize; i++) {
+			arrList[i] = generator.nextInt(201) - 100; // -100 ~ 100까지 범위에 있는 무작위 난수 추출
+			// generator.nextInt() : 매개변수 선언이 없으면 가능한 모든 int 값(양수와 음수)에 걸친 범위의 난수를 반환
+		}
+		System.out.println("BEFORE : " + Arrays.toString(arrList));
+		
+		bubbleSort(arrList);
+		
+		System.out.println("AFTER : " + Arrays.toString(arrList));
+	}
+}
