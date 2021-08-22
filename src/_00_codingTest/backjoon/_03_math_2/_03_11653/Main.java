1e48ab52dffc6fa3495b05d5a187d8770f2c49d4
dlalstj0213 <dlalstj0213@gmail.com>
2021-08-22 18:47:02 +0900

    소인수분해

---
+++ src/_00_codingTest/backjoon/_03_math_2/_03_11653/Main.java
@@ -0,0 +1,23 @@
+package _00_codingTest.backjoon._03_math_2._03_11653;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i * i <= N; i++) {
+            while (N % i == 0) {
+                System.out.println(i);
+                N /= i;
+            }
+        }
+        if (N != 1) {
+            System.out.println(N);
+        }
+    }
+}
